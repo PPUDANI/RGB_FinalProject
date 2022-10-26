8059c29be18085cb78ba3bae981107ae5692a1c8
PPUDANI <zxvk1030@naver.com>
2022-10-26 16:18:06 +0900

    a

---
+++ MetaMeta/Source/MetaMeta/MetangCharacterCPP.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MetangCharacterCPP.h"
+#include <Engine/Classes/Camera/CameraComponent.h>
+#include <Engine/Classes/Components/CapsuleComponent.h>
+#include <Engine/Classes/GameFramework/SpringArmComponent.h>
+// Sets default values
+AMetangCharacterCPP::AMetangCharacterCPP()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TPSCameraSpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("TPSCameraSpringArm"));
+	TPSCameraSpringArmComponent->SetupAttachment(GetCapsuleComponent());
+	TPSCameraSpringArmComponent->SetRelativeLocation(FVector(0.0f, 0.0f, BaseEyeHeight));
+	TPSCameraSpringArmComponent->bUsePawnControlRotation = true;
+
+	TPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TPSCamera"));
+	TPSCameraComponent->SetupAttachment(TPSCameraSpringArmComponent, USpringArmComponent::SocketName);
+}
+
+// Called when the game starts or when spawned
+void AMetangCharacterCPP::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMetangCharacterCPP::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMetangCharacterCPP::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAxis("MoveForward", this, &AMetangCharacterCPP::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &AMetangCharacterCPP::MoveRight);
+	InputComponent->BindAxis("Turn", this, &AMetangCharacterCPP::AddControllerYawInput);
+	InputComponent->BindAxis("LookUp", this, &AMetangCharacterCPP::AddControllerPitchInput);
+}
+
+void AMetangCharacterCPP::MoveForward(float AxisValue)
+{ 
+	AddMovementInput(GetActorForwardVector(), AxisValue);
+}
+
+void AMetangCharacterCPP::MoveRight(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector(), AxisValue);
+}
+
---
+++ MetaMeta/Source/MetaMeta/MetangCharacterCPP.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MetangCharacterCPP.generated.h"
+
+UCLASS()
+class METAMETA_API AMetangCharacterCPP : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMetangCharacterCPP();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void MoveForward(float AxisValue);
+
+	UFUNCTION()
+	void MoveRight(float AxisValue);
+
+	UPROPERTY(VisibleAnyWhere)
+	class USpringArmComponent* TPSCameraSpringArmComponent;
+
+	UPROPERTY(VisibleAnyWhere)
+	class UCameraComponent* TPSCameraComponent;
+};
